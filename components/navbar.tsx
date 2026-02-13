d6462f206937722fdd48c8cb4e0b0a83016935bf
lahalaha <wongkamfatt9@gmail.com>
2026-02-13 21:38:58 +0800

    increase z of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export default async function Navbar() {
 
 
     return (
-        <nav className="border-b p-4 flex items-center justify-between bg-white shadow-sm">
+        <nav className="border-b p-4 z-100 flex items-center justify-between bg-white shadow-sm">
             <div className="flex items-center gap-4">
                 <UserButton />
             </div>
